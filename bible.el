3c48db7a380c7fa0d5016f289defce288bd501ad
Roerick Sweeney <sroerick@gmail.com>
2023-09-03 01:42:10 -0500

    added next chapter and previous chapter functionality

--- bible.el
+++ bible.el
@@ -2,7 +2,7 @@
 
 ;; set variable
 (with-temp-buffer
-  (insert-file-contents "~/bible_web.xml")
+  (insert-file-contents "./bible_web.xml")
   (setq xml-string (buffer-string)))
 
 ;; parse the buffer
@@ -40,7 +40,56 @@
 	      (push bname book-names))))))
     (message "Names of all books: %s" (nreverse book-names))))
 )
-(get_book_list)
+;; (get_book_list)
+
+
+(defun get_book_and_chapter_info ()
+  (let ((root (car parsed-xml))
+        (book-info nil))
+    (let ((children (xml-node-children root)))
+      (dolist (child children)
+        (unless (stringp child)
+          (let ((attributes (xml-node-attributes child)))
+            (let ((bname (cdr (assq 'bname attributes))))
+              (when bname
+                (let ((chapters nil)
+                      (biblebook-children (xml-node-children child)))
+                  (dolist (chapter-node biblebook-children)
+                    (unless (stringp chapter-node)
+                      (let ((chapter-attributes (xml-node-attributes chapter-node)))
+                        (let ((cnumber (cdr (assq 'cnumber chapter-attributes))))
+                          (when cnumber
+                            (push cnumber chapters))))))
+                  (push (cons bname (nreverse chapters)) book-info))))))))
+    (nreverse book-info)))
+
+;; (get_book_and_chapter_info)
+
+
+
+(defun get_book_and_chapter_info ()
+  (let ((root (car parsed-xml))
+        (book-info-alist nil))
+    (let ((children (xml-node-children root)))
+      (dolist (child children)
+        (unless (stringp child)
+          (let ((attributes (xml-node-attributes child)))
+            (let ((bname (cdr (assq 'bname attributes))))
+              (when bname
+                (let ((chapters nil)
+                      (biblebook-children (xml-node-children child)))
+                  (dolist (chapter-node biblebook-children)
+                    (unless (stringp chapter-node)
+                      (let ((chapter-attributes (xml-node-attributes chapter-node)))
+                        (let ((cnumber (cdr (assq 'cnumber chapter-attributes))))
+                          (when cnumber
+                            (push cnumber chapters))))))
+                  (push (cons (intern bname) (nreverse chapters)) book-info-alist))))))))
+    (nreverse book-info-alist)))
+
+(setq book-and-chapters (get_book_and_chapter_info))
+
+
 
 
 (defun format-chapter (chapter-contents)
@@ -57,8 +106,6 @@
 ;; get target chapter
 (defun get_chapter (target-book target-chapter xml)
 (let ((root (car xml))
-      (target-book "Revelation")
-      (target-chapter "1")
       (chapter-contents nil)
       (found nil))
   (dolist (biblebook (xml-get-children root 'BIBLEBOOK) (and found chapter-contents))
@@ -76,13 +123,49 @@
         (insert (format "%s %s: \n\n" target-book target-chapter))
         (insert formatted-chapter)
 	(goto-char (point-min))
+	(set (make-local-variable 'current-book) target-book)
+	(set (make-local-variable 'current-chapter) target-chapter)
         (setq buffer-read-only t))
-    (message "Chapter not found")))
-)
+    (message "Chapter not found"))))
 
-(get_chapter "Genesis" "1" parsed-xml)
+;; (get_chapter "Genesis" "1" parsed-xml)
 ;; cant get this to work
 (define-key dabar-mode-keymap (kbd "C-c b c") 'get_chapter)
+
+
+(defun get-next-chapter ()
+  (interactive)
+  ;; Assuming `current-book` and `current-chapter` are buffer-local variables in the chapter display buffer
+  (let ((next-item (get-next-chapter-item current-chapter book-and-chapters))) ;; Assume book-alist is defined and accessible
+    (if next-item
+        (progn
+          ;; Fetch and display the next chapter
+          (get_chapter (car next-item) (cdr next-item) parsed-xml) ;; Assume parsed-xml is defined and accessible
+          ;; Update buffer-local variables
+          (set (make-local-variable 'current-book) (car next-item))
+          (set (make-local-variable 'current-chapter) (cdr next-item)))
+      (message "You are at the last chapter"))))
+
+(defun get-next-chapter-item (current-item book-info-alist)
+  (let ((found nil)
+        (result nil)
+        (current-book nil))
+    (catch 'done
+      (dolist (pair book-info-alist)
+        (setq current-book (car pair))
+        (dolist (item (cdr pair))
+          (if found
+              (progn
+                (setq result (cons current-book item))
+                (throw 'done t)))
+          (when (equal current-item item)
+            (setq found t)))))
+    (if (not found)
+        (error "Item not found in list"))
+    result))
+;; Keybinding to "turn the page"
+(define-key dabar-mode-keymap (kbd "C-c n") 'get-next-chapter)
+
 
 
 ;; find verse
@@ -105,6 +188,6 @@
 		    (setq found-verse t))))))))))
   (if found-verse
        (message "%s %s:%s. %s" target-book target-chapter target-verse verse-contents)
-       (message "Verse not found")))
-)
-(get_verse "Genesis" "1" "1" parsed-xml)
+       (message "Verse not found"))))
+;; (get_verse "Genesis" "1" "1" parsed-xml)
+
